ca2c5ad0bd88dec64b72b9e4596f8e8e8647c46d
kiranvenom <km060360@gmail.com>
2026-07-04 14:19:50 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.get("/health-check",(req,res) => {
+    res.send({
+	status: "working"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`server running on port: ${port}`)
+})
