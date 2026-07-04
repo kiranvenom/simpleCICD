db4e0b3dc00869a8f34aa87b91d62cdba1f14ea7
kiranvenom <km060360@gmail.com>
2026-07-04 18:46:25 +0530

    added addition api

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require("express");
 const app = express();
 
 const port = 3000;
+app.use(express.json());
 
 app.get("/health-check",(req,res) => {
     res.send({
 	status: "working fine",
 	msg: "dont worry => with github actions , change new 3"
     })
+})
+
+app.post("/add", (req,res) => {
+    const {a, b} = req.body
+    const sum = function add(a,b){
+	return a+b
+    }
+    
+    res.send({sum})
 })
 
 app.listen(port, () => {
